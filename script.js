fcf9188febc576d690d3f51b0bdd3d2ef6d7f743
bobmeix <bobmeix@yahoo.com>
2022-08-17 16:13:22 +0300

    Add last 2 buttons and 2 operator functions

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 function add(...numbers) {
     let result = 0;
-    numbers.forEach (number => result += +number);
+    numbers.forEach(number => result += +number);
     return result;
 }
 
 function subtract(number, ...numbers) {
     let result = number;
-    numbers.forEach (number => result -= +number);
+    numbers.forEach(number => result -= +number);
     return result;
 }
 
 function multiply(...numbers) {
     let result = 1;
-    numbers.forEach (number => result *= +number);
+    numbers.forEach(number => result *= +number);
     return result;
 }
 
 function divide(number, ...numbers) {
     let result = number;
-    numbers.forEach (number => result /= +number);
+    numbers.forEach(number => result /= +number);
     return result;
 }
 
 function exponentiate(base, ...exponents) {
     let result = base;
-    exponents.forEach (number => result **= +number);
+    exponents.forEach(number => result **= +number);
+    return result;
+}
+
+function raiseTwoToPower(...exponents) {
+    let result = 2;
+    exponents.forEach(exponent => result = exponentiate(result, exponent));
+    return result;
+}
+
+function calculateNthRoot(radicand, ...degrees) {
+    let result = radicand;
+    degrees.forEach(radicand => result = exponentiate(result, 1 / radicand));
     return result;
 }
 
@@ -44,7 +56,9 @@ function operate (operator, ...numbers) {
 
 console.log(operate(add, 3, 2));
 console.log(operate(subtract, 3, 2));
-console.log(operate(multiply, 3, 2));
+console.log(operate(multiply, 3, 2, 2));
 console.log(operate(divide, 3, 2));
-console.log(operate(exponentiate, 3, 2));
-console.log(operate(reverseSign, -2))
+console.log(operate(exponentiate, '-3', 3, 3));
+console.log(operate(reverseSign, '-2'))
+console.log(operate(raiseTwoToPower, '-2', '-4'));
+console.log(operate(calculateNthRoot, '256', '2', '4', '2'));
