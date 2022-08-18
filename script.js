93373cb9a3d1c3150cc2deff155ad70ac9047833
bobmeix <bobmeix@yahoo.com>
2022-08-18 18:33:11 +0300

    Add partial button input and display functionality

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+const displayBottom = document.querySelector('.display-bottom');
+const displayTop = document.querySelector('.display-top');
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', readNumberButtonValue);
+})
+const controlButtons = document.querySelectorAll('.control');
+controlButtons.forEach(controlButton => {
+    controlButton.addEventListener('click', readControlButtonValue);
+})
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', readOperatorButtonValue);
+})
+const equalButton = document.querySelector('.equal');
+equalButton.addEventListener('click', readEqualButtonValue);
+
+let displayBottomValue;
+
+function reduceDisplayBottomFontSize() {
+    if (displayBottom.textContent.length >= 34) {
+        numberButtons.forEach(numberButton => {
+            numberButton.removeEventListener('click', readNumberButtonValue);
+        })
+    } else if (displayBottom.textContent.length > 23) {
+        displayBottom.setAttribute('style', 'font-size: 20px');
+    } else if (displayBottom.textContent.length > 17) {
+        displayBottom.setAttribute('style', `font-size: 29.5px`);
+    }
+}
+
+function readNumberButtonValue(e) {
+    if (displayBottom.textContent === '0') {
+        displayBottom.textContent = e.target.innerText;
+    } else if (displayBottom.textContent !== '0') {
+        displayBottom.textContent += e.target.innerText;
+    } 
+    
+    if (e.target.id === 'decimal') {
+        e.target.removeEventListener('click', readNumberButtonValue);
+    }
+
+    console.log(e.target.innerText, `length: ${displayBottom.innerText.length}`);
+
+    reduceDisplayBottomFontSize();
+    return e.target.innerText;
+}
+
+function readControlButtonValue(e) {
+    console.log(e.target.innerText);
+}
+
+function readOperatorButtonValue(e) {
+    console.log(e.target.innerText);
+}
+
+function readEqualButtonValue(e) {
+    console.log(e.target.innerText);
+}
+
 function add(...numbers) {
     let result = 0;
     numbers.forEach(number => result += +number);
@@ -5,7 +65,7 @@ function add(...numbers) {
 }
 
 function subtract(number, ...numbers) {
-    let result = number;
+    let result = +number;
     numbers.forEach(number => result -= +number);
     return result;
 }
@@ -17,13 +77,13 @@ function multiply(...numbers) {
 }
 
 function divide(number, ...numbers) {
-    let result = number;
+    let result = +number;
     numbers.forEach(number => result /= +number);
     return result;
 }
 
 function exponentiate(base, ...exponents) {
-    let result = base;
+    let result = +base;
     exponents.forEach(number => result **= +number);
     return result;
 }
@@ -35,7 +95,7 @@ function raiseTwoToPower(...exponents) {
 }
 
 function calculateNthRoot(radicand, ...degrees) {
-    let result = radicand;
+    let result = +radicand;
     degrees.forEach(degree => result = exponentiate(result, 1 / degree));
     return result;
 }
@@ -44,7 +104,11 @@ function reverseSign(number) {
     return -number;
 }
 
-function operate (operator, ...numbers) {
+function calculatePercent(number) {
+    return +number / 100;
+}
+
+function operate(operator, ...numbers) {
     return operator(...numbers);
 }
 
@@ -57,8 +121,9 @@ function operate (operator, ...numbers) {
 console.log(operate(add, 3, 2));
 console.log(operate(subtract, 3, 2));
 console.log(operate(multiply, 3, 2, 2));
-console.log(operate(divide, 3, 2));
+console.log(operate(divide, '3', 4));
 console.log(operate(exponentiate, '-3', 3, 3));
 console.log(operate(reverseSign, '-2'))
 console.log(operate(raiseTwoToPower, '-2', '-4'));
 console.log(operate(calculateNthRoot, '81', 2, 3, 2));
+console.log(operate(calculatePercent, '25'));
